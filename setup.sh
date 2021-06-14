99896e913415719fbbacc3d71824674dbcd78ed0
Akmal <magchuz@pm.me>
2021-06-14 09:27:43 +0700

    Trying to fix "no space left on device" on setup

--- setup.sh
+++ setup.sh
@@ -10,4 +10,5 @@ fi
 git clone "https://github.com/TheoCoombes/crawlingathome" crawlingathome_client
 pip3 install -r crawlingathome_client/requirements.txt --no-cache-dir
 pip3 install -r requirements.txt --no-cache-dir
+pip3 install tensorflow
 pip3 install git+https://github.com/Wikidepia/CLIP --no-cache-dir
