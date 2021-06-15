e49b510cad641061f27a006c16a31d8d8dee65c8
Christoph Schuhmann <christoph_s@freenet.de>
2021-06-15 23:58:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,13 @@ pip3 install -r crawlingathome_client/requirements.txt --no-cache-dir
 pip3 install -r requirements.txt --no-cache-dir
 pip3 install tensorflow --no-cache-dir
 pip3 install git+https://github.com/Wikidepia/CLIP --no-cache-dir
+
+pip3 install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install shutil
+pip3 install tfr_image
+pip3 install pandas 
+pip3 install pillow
+pip3 install glob
+pip3 install ftfy regex tqdm
+pip3 install git+https://github.com/openai/CLIP.git
+
